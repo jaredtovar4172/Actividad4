695b11abc3018d7ec71b47be753ab213a6a1db93
jaredtovar4172 <80796166+jaredtovar4172@users.noreply.github.com>
2021-03-16 17:46:23 -0600

    Add files via upload

---
+++ Ej4_Centigrados_a_Fahrenheit/Ej4_Centigrados_a_Fahrenheit/Convertir.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej4_Centigrados_a_Fahrenheit
+{
+    class Convertir
+    {
+        public double Temperatura (double c)
+        {
+            double f = (c * 9 / 5) + 32;
+            return (f);
+        }
+    }
+}
---
+++ Ej4_Centigrados_a_Fahrenheit/Ej4_Centigrados_a_Fahrenheit/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ej4_Centigrados_a_Fahrenheit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Convertir x = new Convertir();
+            double celsius = double.Parse(textBox1.Text);
+            double f = x.Temperatura(celsius);
+            this.textBox2.Text = f.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox1.Focus();
+            textBox2.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            DialogResult salir;
+            salir = MessageBox.Show("Deseas salir de la aplicacion?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (salir == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+    }
+
